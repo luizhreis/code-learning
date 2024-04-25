3b8e6b3adf794567aba87a8e5c6aaed2adbd8413
Luiz Reis <luizhreis.gris@gmail.com>
2024-04-25 15:44:38 -0300

    feat: two-sum in rust

--- two-sum/rust/src/main.rs
+++ two-sum/rust/src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+struct Solution {
+    nums: Vec<i32>,
+    target:  i32,
+}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut numbers: HashMap<i32, i32> = HashMap::new();
+
+        for (i, &n) in nums.iter().enumerate() {
+            let dif = target - n;
+            
+            match numbers.get(&dif) {
+                Some(&j) => return vec![j as i32, i as i32],
+                None => numbers.insert(n, i as i32),
+            };
+        }
+        unreachable!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = Solution::two_sum(vec![2,7,11,15],9);
+    print!("{:?}",result);
 }
